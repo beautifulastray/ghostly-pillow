0af92efcc6668a467ea06884cdb73012f348bf62
beautifulastray <katerinka.x.x@gmail.com>
2020-10-29 22:39:45 +0300

    comments

--- img-resize.py
+++ img-resize.py
@@ -11,11 +11,11 @@ files = [x for x in glob('*.jpeg') + glob('*.jpg') + glob('*.png') if not x.ends
 
 def convert_to_srgb(img):
     '''Конвертирует PIL-изображение в цветовое пространство sRGB (если возможно)'''
-    icc = img.info.get('icc_profile')
     #Смотрит какой цветовой профиль имеет изображение
+    icc = img.info.get('icc_profile')
     if icc:
-        io_handle = io.BytesIO(icc)
         #Virtual file - записывает данные ICC в буфер
+        io_handle = io.BytesIO(icc)
         src_profile = ImageCms.ImageCmsProfile(io_handle)
         dst_profile = ImageCms.createProfile('sRGB')
         img = ImageCms.profileToProfile(img, src_profile, dst_profile)
@@ -23,22 +23,22 @@ def convert_to_srgb(img):
 
 def trim(img):
     '''Обрезает пустые поля у изображения (если возможно)'''
-    bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
     #Принимает за фон новое изображение, созданное по образу и подобию исходного, залитое цветом пикселя (0,0)
-    diff = ImageChops.difference(img, bg)
+    bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
     #Вычисляет разницу между исходным изображением и созданным
-    diff = ImageChops.add(diff, diff, 1.0, -17)
+    diff = ImageChops.difference(img, bg)
     #Какая-то магия
-    bbox = diff.getbbox()
+    diff = ImageChops.add(diff, diff, 1.0, -17)
     #Вычисляет рамку ненулевых значений
+    bbox = diff.getbbox()
     if bbox:
-        return img.crop(bbox)
         #Возвращает обрезанное по рамке изображение
+        return img.crop(bbox)
     else:
         return img
 
 def ski_rotate(img):
-    '''Поворачивает изображение, если оно соответствует заданной пророрции'''
+    '''Поворачивает изображение, если оно соответствует заданной пропорции'''
     if img.height / img.width >= 4.8:
         img = img.rotate(270, resample=Image.BICUBIC, expand=1)
     return img
@@ -64,32 +64,32 @@ for infile in files:
             continue
 
         img = trim(img)
-        img.thumbnail((w, h))
         #Подгоняет размер img под рамку заданную в sizes, уменьшает img, если необходимо
+        img.thumbnail((w, h))
         img = ski_rotate(img)
         img = ski_double(img)
 
         if img.mode == 'RGBA': 
-            background = Image.new('RGBA', (img.size), (255, 255, 255, 255))
             #Создает белое фоновое изображение в RGBA
-            img = Image.alpha_composite(background, img)
+            background = Image.new('RGBA', (img.size), (255, 255, 255, 255))
             #Накладывет исходный img на созданный фон
-            img = img.convert('RGB')
+            img = Image.alpha_composite(background, img)
             #Конвертирует png в jpg (RGBA в RGB)
+            img = img.convert('RGB')
 
         if img.mode == 'P':
-            img = img.convert('RGBA')
             #Конвертирует индексированную палитру в RGBA
-            background = Image.new('RGBA', (img.size), (255, 255, 255, 255))
+            img = img.convert('RGBA')
             #Создает белое фоновое изображение в RGBA
-            img = Image.alpha_composite(background, img)
+            background = Image.new('RGBA', (img.size), (255, 255, 255, 255))
             #Накладывет исходный img на созданный фон
-            img = img.convert('RGB')
+            img = Image.alpha_composite(background, img)
             #Конвертирует png в jpg (RGBA в RGB)
+            img = img.convert('RGB')
 
         path = infile
-        root_ext = os.path.splitext(path)
         #Разбивает имя файла на две части: до точки(root или [1]) и после(ext или [0])
-        img.save((os.path.join(r'/Users/Katie/resizes/result', root_ext[0])) + '-resized.jpg', 'JPEG', quality = 100)
+        root_ext = os.path.splitext(path)
         #Cохраняет изображение с новым именем и качеством 100 в формате JPEG
+        img.save((os.path.join(r'/Users/Katie/resizes/result', root_ext[0])) + '-resized.jpg', 'JPEG', quality = 100)
 print("Succsess!")
